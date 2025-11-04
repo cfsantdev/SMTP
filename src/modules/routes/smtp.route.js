816c7dd420eb035f407b37ffa5d625949384e971
CAIO SANTANA <cfsant4gamers@gmail.com>
2025-11-04 19:06:55 -0300

    Remove nodemailer module reference

--- src/modules/routes/smtp.route.js
+++ src/modules/routes/smtp.route.js
@@ -1,7 +1,6 @@
 
 import 'dotenv/config';
 import MailTemplate from '../config/mail.template.js';
-import NodeMailer from 'nodemailer';
 import { Resend } from 'resend';
 import { v4 as uuidv4 } from 'uuid';
 
