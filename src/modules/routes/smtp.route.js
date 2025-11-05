9d471c20987b48af76e83414af6ac311d36273c2
CAIO SANTANA <cfsant4gamers@gmail.com>
2025-11-05 08:54:58 -0300

    Implementação smtp.service.js

--- src/modules/routes/smtp.route.js
+++ src/modules/routes/smtp.route.js
@@ -1,6 +1,6 @@
 
 import 'dotenv/config';
-import MailTemplate from '../config/mail.template.js';
+import SmtpService from '../smtp.service.js';
 import { Resend } from 'resend';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -12,7 +12,6 @@ class SmtpRoute {
 
     config = function(){
         this.app.post('/smtp', async (req, res) => {
-            let id = uuidv4();
             try {
                 const { email, nome } = req.body;
 
@@ -20,41 +19,28 @@ class SmtpRoute {
                 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
                 
                 if (!email) {
-                    return res.status(400).json({ error: 'O campo email é obrigatório.' });
+                    throw new Error('O campo email é obrigatório.');
                 }
             
                 if (!emailRegex.test(email)) {
-                    return res.status(400).json({ error: 'O email fornecido é inválido.' });
+                    throw new Error('O email é fornecido é inválido.');
                 }
-                
-                await this.send(req, res, email, nome, id);
-            } catch (err) {
-                console.log('Nodemailer(400): { "uuid": "' + id + '"  "email": "' + email + '", "nome": "' + nome + '" }');
-                res.status(400).json({ error: 'Erro ao enviar email: ' + err })
-            }
-        });
-    }
 
-    send = async function(req, res, email, nome, id) {
-        return new Promise(async (resolve, reject) => {
-            const resend = new Resend(process.env.SMTP_KEY);
-            
-            await resend.emails.send({
-                from: process.env.SMTP_USER,
-                to: email,
-                subject: 'VC NUTRIÇÃO ESPORTIVA - CONFIRMAÇÃO DE COMPRA',
-                html: new MailTemplate(nome,id).get(),
-            }).then((data) => {
-                if(data.error){
-                    console.log('Nodemailer(400): { "uuid": "' + id + '"  "email": "' + email + '", "nome": "' + nome + '", "erro": "' + JSON.stringify(data.error) + '" }');
-                    res.status(data.error.statusCode).json({ error: 'Erro ao enviar email: ' + data.error.message });
+                const {data, id} = await SmtpService.send(email, nome);
+                if(data === null){
+                    throw new Error('Erro ao consumir o serviço SMTP.');
                 }
+
+                if(id === null){
+                    throw new Error('Erro ao consumir o serviço SMTP.');
+                }
+
                 console.log('Nodemailer(200): { "uuid": "' + id + '"  "data": "' + JSON.stringify(data) + '" }');
-                res.status(200).json({ uuid: id, email, nome: nome })
-            }).catch((err) => {
-                console.log('Nodemailer(400): { "uuid": "' + id + '"  "email": "' + email + '", "nome": "' + nome + '", "erro": "' + JSON.stringify(err) + '" }');
-                res.status(400).json({ error: 'Erro ao enviar email: ' + err })
-            });
+                res.status(200).json({ uuid: id, email, nome: nome });
+            } catch (err) {
+                console.log('Nodemailer(500): { "email": "' + email + '", "nome": "' + nome + '", "error": "' + err + '" }');
+                res.status(500).json({ error: err })
+            }
         });
     }
 }
--- src/modules/smtp.service.js
+++ src/modules/smtp.service.js
@@ -1,2 +1,28 @@
-require('dotenv/config');
-const NODEMAILER = require('nodemailer');
+import 'dotenv/config';
+import MailTemplate from './config/mail.template.js';
+import { v4 as uuidv4 } from 'uuid';
+import { Resend } from 'resend';
+
+class SmtpService {
+    static send = async function(email, nome) {
+        return new Promise(async (resolve, reject) => {
+            const id = uuidv4();
+            const resend = new Resend(process.env.SMTP_KEY);
+            
+            const { data, error } = await resend.emails.send({
+                from: process.env.SMTP_USER,
+                to: email,
+                subject: 'VC NUTRIÇÃO ESPORTIVA - CONFIRMAÇÃO DE COMPRA',
+                html: new MailTemplate(nome,id).get(),
+            });
+            
+            if (error) {
+                reject(new { data: { error: error }, id: null });
+            }
+
+            resolve({ data: data, id: id });
+        });
+    }
+}
+
+export default SmtpService;
